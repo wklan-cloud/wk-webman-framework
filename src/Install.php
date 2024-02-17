32081d5112c6969e342653d629903d1f03e94732
euper <nginx11724@qq.com>
2024-02-17 09:51:19 +0800

    修改文件命名规范及路径调整

---
+++ src/Install.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Webman;
+
+class Install
+{
+    const WEBMAN_PLUGIN = true;
+
+    /**
+     * @var array
+     */
+    protected static $pathRelation = [
+        'start.php' => 'start.php',
+        'windows.php' => 'windows.php',
+        'core/support/bootstrap.php' => 'core/support/Bootstrap.php',
+        'core/support/helpers.php' => 'core/support/Helpers.php',
+    ];
+
+    /**
+     * Install
+     * @return void
+     */
+    public static function install()
+    {
+        static::installByRelation();
+    }
+
+    /**
+     * Uninstall
+     * @return void
+     */
+    public static function uninstall()
+    {
+
+    }
+
+    /**
+     * InstallByRelation
+     * @return void
+     */
+    public static function installByRelation()
+    {
+        foreach (static::$pathRelation as $source => $dest) {
+            if ($pos = strrpos($dest, '/')) {
+                $parentDir = base_path() . '/' . substr($dest, 0, $pos);
+                if (!is_dir($parentDir)) {
+                    mkdir($parentDir, 0777, true);
+                }
+            }
+            $sourceFile = __DIR__ . "/$source";
+            copy_dir($sourceFile, base_path() . "/$dest", true);
+            echo "Create $dest\r\n";
+            if (is_file($sourceFile)) {
+                @unlink($sourceFile);
+            }
+        }
+    }
+
+}
